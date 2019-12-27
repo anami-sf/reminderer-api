b28f4826045e0f94cd24d8a449e42c59ba50515c
anami823 <anami823@gmail.com>
2019-12-26 18:26:48 -0800

    Update notification controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,15 +2,18 @@ require 'twilio-ruby'
 
 class NotificationsController < ApplicationController
 
-    skip_before_action :verify_authenticity_token
+    #skip_before_action :verify_authenticity_token
 
-    def notify
+    @client = Twilio::REST::Client.new(
+        Rails.application.credentials.twilio_account_sid,
+        Rails.application.credentials.twilio_auth_token
+        )
+        
+    @client.messages.create(
+    from: '+14152134974',
+    to: '+14153351196',
+    body: 'Sending this message from my new twilio app. If you got it, please reply. - your gf',
+    #media_url: 'http://linode.rabasa.com/yoda.gif'
+    )
 
-        twiml = Twilio::TwiML::MessagingResponse.new do |r|
-            r.message body: 'The Robots are coming! Head for the hills!'
-        end
-
-        render :xml => twiml.to_xml
-    end
-  
 end
--- config/initializers/twilio.rb
+++
@@ -1,6 +0,0 @@
-require 'twilio-ruby'
-
-Twilio.configure do |config|
-    config.account_sid = Rails.application.credentials.twilio_account_sid
-    config.auth_token = Rails.application.credentials.twilio_auth_token
-end
