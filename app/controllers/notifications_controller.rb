7a4b19ea8203e7b41e4f747471ef2195de653417
anami823 <anami823@gmail.com>
2019-12-26 17:17:10 -0800

    Update create method in twilio notify controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -10,7 +10,7 @@ class NotificationsController < ApplicationController
             Rails.application.credentials.twilio_auth_token
         )
 
-        @message = @client.messages.create(
+        @client.messages.create(
             from: '+14152134974', 
             to: '+14153351196', 
             body: 'Sending this message from my new twilio app. If you got it, please reply. - your gf',
