8e3bd7876a4f944ad55e1f19f2aa670b6c9a1c81
anami823 <anami823@gmail.com>
2019-12-26 17:38:06 -0800

    reconfigure notifications controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,21 +5,12 @@ class NotificationsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def notify
-        @client = Twilio::REST::Client.new(
-            Rails.application.credentials.twilio_account_sid,
-            Rails.application.credentials.twilio_auth_token
-        )
 
-        @client.messages.create(
-            from: '+14152134974', 
-            to: '+14153351196', 
-            body: 'Sending this message from my new twilio app. If you got it, please reply. - your gf',
-            #media_url: 'http://linode.rabasa.com/yoda.gif'
-        )
-        
-        #puts @message.sid
-        #render plain: message.status
-        #render :xml => @message.to_xml
+        twiml = Twilio::TwiML::MessagingResponse.new do |r|
+            r.message body: 'The Robots are coming! Head for the hills!'
+        end
+
+        render :xml => twiml.to_xml
     end
   
 end
