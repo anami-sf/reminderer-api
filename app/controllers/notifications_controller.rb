cb0ec5636956976b9904b2ab999395ebe85d83c4
anami823 <anami823@gmail.com>
2019-12-27 09:05:08 -0800

    Update db yml

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -14,11 +14,11 @@ class NotificationsController < ApplicationController
             config.auth_token = Rails.application.credentials.twilio_auth_token
         end
         
-    @message = @client.messages.create(
+    message = @client.messages.create(
     from: '+14152134974',
     to: '+14153351196',
     body: 'Sending this message from my new twilio app. If you got it, please reply. - your gf',
     #media_url: 'http://linode.rabasa.com/yoda.gif'
     )
-    render plain: @message.status
+    render plain: message.status
 end
